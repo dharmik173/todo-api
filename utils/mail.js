bce853557ed41fa6f98f35ebc71b7e9d1475ce63
dharmikshah189 <dharmikshah189@gmail.com>
2025-12-23 18:06:26 +0000

    change env

--- utils/mail.js
+++ utils/mail.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_SECURE === 'true', // Convert to boolean
+    secure: false, // Convert to boolean
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
